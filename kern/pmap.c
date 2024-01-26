85ef1b117670f5ab2690791571f89ab73c86e231
Yunpeng <mashed.potato.mine@gmail.com>
2024-01-25 20:12:39 -0500

    lab3 exercise9

--- kern/pmap.c
+++ kern/pmap.c
@@ -563,8 +563,18 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
-
-	return 0;
+    for(int i = 0; i < len; i += PGSIZE) {
+        if((uintptr_t) va + i >= ULIM) {
+            user_mem_check_addr = (uintptr_t) va + i;
+            return -E_FAULT;
+        }
+        pte_t* pte = pgdir_walk(env->env_pgdir, (void *) ((uintptr_t) va + i), 0);
+        if(pte == NULL || (*pte & perm) != (perm | PTE_P)) {
+            user_mem_check_addr = (uintptr_t) va + i;
+            return -E_FAULT;
+        }
+    }
+    return 0;
 }
 
 //

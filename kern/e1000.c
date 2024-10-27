b32ce41dc96b606dd521bd49f6f8aee2365fa1cc
Yunpeng <mashed.potato.mine@gmail.com>
2024-10-27 15:49:48 -0700

    lab6 exercise3

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,3 +2,10 @@
 #include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+int
+e1000_attach(struct pci_func *pcif)
+{
+    pci_func_enable(pcif);
+    cprintf("e1000_attach: attaching e1000\n");
+    return 0;
+}

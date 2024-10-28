916bb4f1117c1f96af3c6350063570982bf6290f
Yunpeng <mashed.potato.mine@gmail.com>
2024-10-27 21:03:10 -0700

    lab6 exercise5

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,8 +1,16 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
+#include <inc/string.h>
 
 
 volatile uint32_t * e1000_mmio_base_addr;
+
+#define TX_BUF_SIZE 1536  // 16-byte aligned for performance
+#define NTXDESC     64
+
+static struct tx_desc e1000_tx_queue[NTXDESC] __attribute__((aligned(16)));
+static uint8_t e1000_tx_buf[NTXDESC][TX_BUF_SIZE];
+
 
 // LAB 6: Your driver code here
 int
@@ -15,5 +23,35 @@ e1000_attach(struct pci_func *pcif)
     e1000_mmio_base_addr = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
     cprintf("e1000_attach: status %x\n", E1000_REG(E1000_STATUS_OFFSET));
 
+    e1000_tx_init();
+
     return 0;
+}
+
+static void
+e1000_tx_init()
+{
+    // initialize tx queue
+    int i;
+    memset(e1000_tx_queue, 0, sizeof(e1000_tx_queue));
+    for (i = 0; i < NTXDESC; i++) {
+        e1000_tx_queue[i].addr = PADDR(e1000_tx_buf[i]);
+    }
+
+    // initialize transmit descriptor registers
+    E1000_REG(E1000_TDBAL) = PADDR(e1000_tx_queue);
+    E1000_REG(E1000_TDBAH) = 0;
+    E1000_REG(E1000_TDLEN) = sizeof(e1000_tx_queue);
+    E1000_REG(E1000_TDH) = 0;
+    E1000_REG(E1000_TDT) = 0;
+
+    // initialize transmit control registers
+    E1000_REG(E1000_TCTL) &= ~(E1000_TCTL_CT | E1000_TCTL_COLD);
+    E1000_REG(E1000_TCTL) |= E1000_TCTL_EN | E1000_TCTL_PSP |
+                             (E1000_COLLISION_THRESHOLD << E1000_CT_SHIFT) |
+                             (E1000_COLLISION_DISTANCE << E1000_COLD_SHIFT);
+    E1000_REG(E1000_TIPG) &= ~(E1000_TIPG_IPGT_MASK | E1000_TIPG_IPGR1_MASK | E1000_TIPG_IPGR2_MASK);
+    E1000_REG(E1000_TIPG) |= E1000_DEFAULT_TIPG_IPGT |
+                             (E1000_DEFAULT_TIPG_IPGR1 << E1000_TIPG_IPGR1_SHIFT) |
+                             (E1000_DEFAULT_TIPG_IPGR2 << E1000_TIPG_IPGR2_SHIFT);
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -13,4 +13,51 @@ e1000_attach(struct pci_func *pcif);
 #define E1000_STATUS_OFFSET 0x00008
 #define E1000_REG(offset) (*(volatile uint32_t*)(e1000_mmio_base_addr + offset / 4))
 
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+
+/* Transmit Control */
+#define E1000_TCTL_EN   0x00000002  /* enable tx */
+#define E1000_TCTL_PSP  0x00000008  /* pad short packets */
+#define E1000_TCTL_CT   0x00000ff0  /* collision threshold */
+#define E1000_TCTL_COLD 0x003ff000  /* collision distance */
+
+/* Collision related configuration parameters */
+#define E1000_COLLISION_THRESHOLD   0x10
+#define E1000_CT_SHIFT              4
+
+/* Collision distance is a 0-based value that applies to half-duplex-capable hardware only. */
+#define E1000_COLLISION_DISTANCE    0x40
+#define E1000_COLD_SHIFT            12
+
+/* Default values for the transmit IPG register */
+#define E1000_DEFAULT_TIPG_IPGT	    10
+#define E1000_DEFAULT_TIPG_IPGR1    4
+#define E1000_DEFAULT_TIPG_IPGR2    6
+#define E1000_TIPG_IPGT_MASK        0x000003FF
+#define E1000_TIPG_IPGR1_MASK       0x000FFC00
+#define E1000_TIPG_IPGR2_MASK       0x3FF00000
+#define E1000_TIPG_IPGR1_SHIFT      10
+#define E1000_TIPG_IPGR2_SHIFT      20
+
+static void
+e1000_tx_init();
+
+struct tx_desc
+{
+    uint64_t addr;
+    uint16_t length;
+    uint8_t cso;
+    uint8_t cmd;
+    uint8_t status;
+    uint8_t css;
+    uint16_t special;
+};
+
+
 #endif  // SOL >= 6

e1991cb146d6bde6ee2285c3e10e781284c4c3fe
Yunpeng <mashed.potato.mine@gmail.com>
2024-10-27 18:40:06 -0700

    lab6 exercise4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,11 +1,19 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
+
+volatile uint32_t * e1000_mmio_base_addr;
+
 // LAB 6: Your driver code here
 int
 e1000_attach(struct pci_func *pcif)
 {
-    pci_func_enable(pcif);
     cprintf("e1000_attach: attaching e1000\n");
+
+    pci_func_enable(pcif);
+
+    e1000_mmio_base_addr = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("e1000_attach: status %x\n", E1000_REG(E1000_STATUS_OFFSET));
+
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -10,5 +10,7 @@
 int
 e1000_attach(struct pci_func *pcif);
 
+#define E1000_STATUS_OFFSET 0x00008
+#define E1000_REG(offset) (*(volatile uint32_t*)(e1000_mmio_base_addr + offset / 4))
 
 #endif  // SOL >= 6

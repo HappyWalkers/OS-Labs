409a5edde0ff69ffbb73580448fb67f9bdc54c09
Yunpeng <mashed.potato.mine@gmail.com>
2024-11-02 13:22:27 -0700

    lab6 exercise7

--- kern/syscall.c
+++ kern/syscall.c
@@ -12,6 +12,7 @@
 #include <kern/console.h>
 #include <kern/sched.h>
 #include <kern/time.h>
+#include <kern/e1000.h>
 
 // Print a string to the system console.
 // The string is exactly 'len' characters long.
@@ -437,6 +438,14 @@ sys_time_msec(void)
     return time_msec();
 }
 
+// transmit a package from user space
+static int
+sys_transmit_packet(void *packet, size_t len)
+{
+    user_mem_assert(curenv, packet, len, PTE_U | PTE_P);
+    return e1000_transmit(packet, len);
+}
+
 // Dispatches to the correct kernel function, passing the arguments.
 int32_t
 syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, uint32_t a5)
@@ -485,6 +494,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
             return sys_env_set_trapframe((envid_t)a1, (struct Trapframe *)a2);
         case SYS_time_msec:
             return sys_time_msec();
+        case SYS_transmit_packet:
+            return sys_transmit_packet((void *)a1, (size_t)a2);
         default:
             return -E_INVAL;
 	}
